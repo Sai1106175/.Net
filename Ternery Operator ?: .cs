47d8d5ce86aa59f565563f8ee3159e2183983bf9
Sharan <32306006+Sai1106175@users.noreply.github.com>
2024-12-16 06:47:29 +0530

    Create  Ternery Operator ?: .cs

---
+++ Ternery Operator ?: .cs
@@ -0,0 +1,18 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		// Ternery Operator ?:
+		
+		int Value = 10;
+		
+		bool isNumberis10 = Value == 10 ? true : false;
+		
+		Console.WriteLine("The Given number is 10 and its {0}",isNumberis10);
+		
+		
+		
+		}
+}
